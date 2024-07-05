30176d63f3d521c1cb83e8cac70ee38e40604f08
Luna Razzaghipour <luna@xoria.org>
2024-07-05 17:38:22 +1000

    Refactor `main()` to use `NSApplicationDelegate`

--- entry_point.m
+++ entry_point.m
@@ -4,59 +4,67 @@
 
 #include "main_view.m"
 
-static NSString *
-AppName(void)
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+@implementation AppDelegate
 {
-	NSBundle *bundle = [NSBundle mainBundle];
-	return [bundle objectForInfoDictionaryKey:(__bridge NSString *)kCFBundleNameKey];
+	NSWindow *window;
 }
 
-static NSMenu *
-CreateMenu(void)
+- (void)applicationDidFinishLaunching:(NSNotification *)notification
 {
-	NSMenu *menuBar = [[NSMenu alloc] init];
+	[self createMainMenu];
+
+	NSRect rect = NSMakeRect(100, 100, 500, 400);
+	NSWindowStyleMask style = NSWindowStyleMaskTitled | NSWindowStyleMaskResizable |
+	                          NSWindowStyleMaskClosable | NSWindowStyleMaskMiniaturizable;
+	window = [[NSWindow alloc] initWithContentRect:rect
+	                                     styleMask:style
+	                                       backing:NSBackingStoreBuffered
+	                                         defer:NO];
+
+	MainView *view = [[MainView alloc] initWithFrame:rect];
+	window.contentView = view;
+
+	[window makeKeyAndOrderFront:nil];
+
+	[NSApp activate];
+}
+
+- (void)createMainMenu
+{
+	NSMenu *mainMenu = [[NSMenu alloc] init];
 
 	NSMenuItem *appMenuItem = [[NSMenuItem alloc] init];
-	[menuBar addItem:appMenuItem];
+	[mainMenu addItem:appMenuItem];
 
 	NSMenu *appMenu = [[NSMenu alloc] init];
 	appMenuItem.submenu = appMenu;
 
-	NSString *quitMenuItemTitle = [NSString stringWithFormat:@"Quit %@", AppName()];
+	NSString *quitMenuItemTitle = [NSString stringWithFormat:@"Quit %@", [self appName]];
 	NSMenuItem *quitMenuItem = [[NSMenuItem alloc] initWithTitle:quitMenuItemTitle
 	                                                      action:@selector(terminate:)
 	                                               keyEquivalent:@"q"];
 
 	[appMenu addItem:quitMenuItem];
 
-	return menuBar;
+	NSApp.mainMenu = mainMenu;
 }
+
+- (NSString *)appName
+{
+	NSBundle *bundle = NSBundle.mainBundle;
+	return [bundle objectForInfoDictionaryKey:(__bridge NSString *)kCFBundleNameKey];
+}
+
+@end
 
 int
 main(void)
 {
-	@autoreleasepool
-	{
-		[NSApplication sharedApplication];
-		[NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-
-		NSApp.mainMenu = CreateMenu();
-
-		NSRect rect = NSMakeRect(100, 100, 500, 400);
-
-		NSWindowStyleMask style = NSWindowStyleMaskTitled | NSWindowStyleMaskResizable |
-		                          NSWindowStyleMaskClosable |
-		                          NSWindowStyleMaskMiniaturizable;
-		NSWindow *window = [[NSWindow alloc] initWithContentRect:rect
-		                                               styleMask:style
-		                                                 backing:NSBackingStoreBuffered
-		                                                   defer:NO];
-
-		MainView *view = [[MainView alloc] initWithFrame:rect];
-		window.contentView = view;
-
-		[window makeKeyAndOrderFront:nil];
-		[NSApp activateIgnoringOtherApps:YES];
-		[NSApp run];
-	}
+	[NSApplication sharedApplication];
+	AppDelegate *appDelegate = [[AppDelegate alloc] init];
+	NSApp.delegate = appDelegate;
+	[NSApp run];
 }
